e554433c67041d7a9a863610a2759ca8615153cd
LebohangMhlane <75252307+LebohangMhlane@users.noreply.github.com>
2023-10-09 16:30:41 +0200

    current configs

--- loverfly/loverfly-settings/settings.py
+++ loverfly/loverfly-settings/settings.py
@@ -95,23 +95,34 @@ WSGI_APPLICATION = "loverfly-settings.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "loverflydb",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": ""
+    }
+}
+
 # DATABASES = {
 #     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
+#         "ENGINE": "django.db.backends.mysql",
 #         "NAME": "loverflydb",
-#         "USER": "postgres",
+#         "USER": "mysql  ",
 #         "PASSWORD": "password",
 #         "HOST": "localhost",
 #         "PORT": ""
 #     }
 # }
 
-DATABASES = {
-    "default": {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     "default": {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
