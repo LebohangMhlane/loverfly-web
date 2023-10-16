cbce71741e393c9d43910cc2b068ef51023beb55
LebohangMhlane <75252307+LebohangMhlane@users.noreply.github.com>
2023-10-16 15:58:13 +0200

    import fix

--- loverfly/posts/models.py
+++ loverfly/posts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
 from couples.models import Couple
-from loverfly.likes.models import Liker
+from likes.models import Liker
 
 # Create your models here.
 
