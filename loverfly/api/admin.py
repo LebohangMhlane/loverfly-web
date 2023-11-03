80421a8dd3880d96f30254dcc2d888a177c03ba5
LebohangMhlane <75252307+LebohangMhlane@users.noreply.github.com>
2023-11-03 16:43:09 +0200

    admin changes

--- loverfly/api/admin.py
+++ loverfly/api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from comments.models import CommentLike, Comment
+from comments.models import CommentLike, Comment, CommentReply, CommentReplyLike
 from couples.models import Couple
 from admirers.models import Admirer
 from likes.models import Liker
@@ -14,6 +14,8 @@ admin.site.register(Admirer)
 admin.site.register(Liker)
 admin.site.register(Comment)
 admin.site.register(CommentLike)
+admin.site.register(CommentReply)
+admin.site.register(CommentReplyLike)
 admin.site.register(ProfilePicture)
 
 
